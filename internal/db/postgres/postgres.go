79a8b2e92c4ce0efcdc45df51a1c3cf77a6c1e92
Idris Akkulov <i.akkulov@gmail.com>
2024-03-04 14:05:40 +0300

    connection to postgres

---
+++ internal/db/postgres/postgres.go
@@ -0,0 +1,53 @@
+package postgres
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+
+	"github.com/Nahbox/pg-clickhouse-nats-redis-service/internal/config"
+)
+
+func New(conf *config.PgConfig) (*sql.DB, error) {
+	dsn := conf.Dsn()
+
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("database connection established")
+
+	driver, err := postgres.WithInstance(conn, &postgres.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	migrationsFilePath := fmt.Sprintf("file://%s", conf.PgMigrationsPath)
+
+	m, err := migrate.NewWithDatabaseInstance(migrationsFilePath, "postgres", driver)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Up()
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return nil, err
+	}
+
+	log.Info("database migrated")
+
+	return conn, nil
+}
