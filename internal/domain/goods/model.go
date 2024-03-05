fb760fb9072573bd829bac031b7a2c21cb95b7c4
Idris Akkulov <i.akkulov@gmail.com>
2024-03-05 19:55:38 +0300

    goods models

--- internal/domain/goods/model.go
+++ internal/domain/goods/model.go
@@ -1,29 +1,53 @@
 package goods
 
 type Project struct {
-	Id        int    `json:"-"`
-	Name      string `json:"name"`
-	CreatedAt string `json:"created_at"`
+	Id        int
+	Name      string
+	CreatedAt string
 }
 
 type Good struct {
-	Id          int    `json:"-"`
-	ProjectId   int    `json:"project_id"`
+	Id          int    `json:"id"`
+	ProjectId   int    `json:"projectId"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Priority    int    `json:"priority"`
 	Removed     bool   `json:"removed"`
-	CreatedAt   string `json:"created_at"`
+	CreatedAt   string `json:"createdAt"`
 }
 
 type Meta struct {
-	Total   int
-	Removed int
-	Limit   int
-	Offset  int
+	Total   int `json:"total"`
+	Removed int `json:"removed"`
+	Limit   int `json:"limit"`
+	Offset  int `json:"offset"`
 }
 
 type GetResponse struct {
-	Meta  Meta
-	Goods []Good
+	Meta  Meta   `json:"meta"`
+	Goods []Good `json:"goods"`
+}
+
+type PatchData struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type PostData struct {
+	Name string `json:"name"`
+}
+
+type DeleteResponse struct {
+	Id         int  `json:"id"`
+	CampaignId int  `json:"campaignId"`
+	Removed    bool `json:"removed"`
+}
+
+type ReprioritizeResponse struct {
+	Priorities []Priorities `json:"priorities"`
+}
+
+type Priorities struct {
+	Id       int `json:"id"`
+	Priority int `json:"priority"`
 }
