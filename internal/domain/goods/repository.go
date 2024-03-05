b5c3a6e383b89a6121749cf672463eb5db04081d
Idris Akkulov <i.akkulov@gmail.com>
2024-03-05 19:56:21 +0300

    goods repository interface

--- internal/domain/goods/repository.go
+++ internal/domain/goods/repository.go
@@ -1,12 +1,9 @@
 package goods
 
-import (
-	"context"
-)
-
 type Repository interface {
 	Add(data *Good) (*Good, error)
-	Get(limit, offset int) (GetResponse, error)
+	Get(limit, offset int) (*GetResponse, error)
 	Update(data *Good) (*Good, error)
-	Delete(ctx context.Context) error
+	Delete(id, projectId int) (*DeleteResponse, error)
+	UpdatePriority(id, projectId, newPriority int) (*ReprioritizeResponse, error)
 }
