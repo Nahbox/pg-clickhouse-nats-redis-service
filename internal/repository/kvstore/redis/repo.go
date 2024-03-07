0a815b5d34023071aecd45758cbdd8165fd504b5
Idris Akkulov <i.akkulov@gmail.com>
2024-03-07 03:32:57 +0300

    kvstore redis repository

--- internal/repository/kvstore/redis/repo.go
+++ internal/repository/kvstore/redis/repo.go
@@ -1,1 +1,55 @@
 package redis
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+
+	"github.com/Nahbox/pg-clickhouse-nats-redis-service/internal/domain/goods"
+	"github.com/Nahbox/pg-clickhouse-nats-redis-service/internal/domain/kvstore"
+)
+
+type Repo struct {
+	storage *redis.Client
+}
+
+func NewKVStoreRepo(storage *redis.Client) kvstore.Repository {
+	return &Repo{storage}
+}
+
+func (r *Repo) Set(ctx context.Context, key string, value interface{}) error {
+	// Сериализация значения в байтовый вид
+	encodedValue, err := json.Marshal(value)
+	if err != nil {
+		fmt.Println("Ошибка при сериализации значения:", err)
+		return err
+	}
+
+	// Запись данных в Redis с истекающим сроком годности (время жизни одна минута)
+	err = r.storage.Set(ctx, key, encodedValue, time.Minute).Err()
+	if err != nil {
+		fmt.Println("Ошибка при установке значения в Redis:", err)
+		return err
+	}
+	return nil
+}
+
+func (r *Repo) GetList(ctx context.Context, key string) (*goods.GetResponse, error) {
+	var res *goods.GetResponse
+
+	// Получаем значение для данного ключа
+	val, err := r.storage.Get(ctx, key).Bytes()
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(val, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
