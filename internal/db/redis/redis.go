50daaf5ebd00be7e2b7ae629324ee8dd266e8198
Idris Akkulov <i.akkulov@gmail.com>
2024-03-07 03:32:11 +0300

    connection to redis

--- internal/db/redis/redis.go
+++ internal/db/redis/redis.go
@@ -1,1 +1,25 @@
 package redis
+
+import (
+	"context"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func New() (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	// Проверяем соединение с Redis
+	pong, err := client.Ping(context.Background()).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	log.Info("Подключение к Redis установлено:", pong)
+
+	return client, nil
+}
