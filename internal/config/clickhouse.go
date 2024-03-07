428ef82915667dc2f9bbe27eb6a10093c44c75c3
Idris Akkulov <i.akkulov@gmail.com>
2024-03-07 22:54:42 +0300

    clickhouse config file

--- internal/config/clickhouse.go
+++ internal/config/clickhouse.go
@@ -1,1 +1,21 @@
 package config
+
+import "fmt"
+
+type CHConfig struct {
+	ChHost           string `envconfig:"CLICKHOUSE_HOST" required:"true"`
+	ChPort           int    `envconfig:"CLICKHOUSE_PORT" required:"true"`
+	ChUser           string `envconfig:"CLICKHOUSE_USER" required:"true"`
+	ChPassword       string `envconfig:"CLICKHOUSE_PASSWORD" required:"true"`
+	CHMigrationsPath string `envconfig:"CLICKHOUSE_MIGRATIONS_PATH" required:"true"`
+}
+
+func (ch *CHConfig) ChDsn() string {
+	return fmt.Sprintf("clickhouse://%s:%d?username=%s&password=%s",
+		ch.ChHost, ch.ChPort, ch.ChUser, ch.ChPassword)
+}
+
+func (ch *CHConfig) ChMigrationsPathStr() string {
+	return fmt.Sprintf("file://%s",
+		ch.CHMigrationsPath)
+}
