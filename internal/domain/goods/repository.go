8ff3150edc060e6ca818cf29db9d8244847da9bd
Idris Akkulov <i.akkulov@gmail.com>
2024-03-08 15:09:31 +0300

    goods repository interface

--- internal/domain/goods/repository.go
+++ internal/domain/goods/repository.go
@@ -4,6 +4,6 @@ type Repository interface {
 	Add(data *Good) (*Good, error)
 	Get(limit, offset int) (*GetResponse, error)
 	Update(data *Good) (*Good, error)
-	Delete(id, projectId int) (*DeleteResponse, error)
-	UpdatePriority(id, projectId, newPriority int) (*ReprioritizeResponse, error)
+	Delete(id, projectId int) (*DeleteResponse, *Good, error)
+	UpdatePriority(id, projectId, newPriority int) (*ReprioritizeResponse, []Good, error)
 }
